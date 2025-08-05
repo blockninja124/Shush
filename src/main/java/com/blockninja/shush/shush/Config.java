e376e84fd0cdf2e365b21268a5dafb7da6b0eee8
blockninja124 <haganlgray@gmail.com>
2025-08-05 19:06:57 -0400

    Fix config

--- src/main/java/com/blockninja/shush/shush/Config.java
+++ src/main/java/com/blockninja/shush/shush/Config.java
@@ -1,29 +1,18 @@
 package com.blockninja.shush.shush;
 
-import com.mojang.logging.LogUtils;
-import net.minecraft.world.item.Item;
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.fml.event.config.ModConfigEvent;
-import net.neoforged.neoforge.common.ModConfigSpec;
-import org.slf4j.Logger;
-
 import java.util.List;
-import java.util.Set;
 import java.util.regex.Pattern;
-import java.util.regex.PatternSyntaxException;
-
+import net.minecraftforge.common.ForgeConfigSpec;
 
 public class Config {
-    private static final Logger LOGGER = LogUtils.getLogger();
 
-    private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
+    private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
 
-    public static final ModConfigSpec.ConfigValue<List<? extends String>> MATCHED_LOGS =
+    public static final ForgeConfigSpec.ConfigValue<List<? extends String>> MATCHED_LOGS =
             BUILDER.comment("Regex patterns to remove from logs")
-            .defineListAllowEmpty("matchedLogs", List.of(), () -> "\\w+", Config::validateString);
+            .defineListAllowEmpty("matchedLogs", List.of(), Config::validateString);
 
-    static final ModConfigSpec SPEC = BUILDER.build();
+    static final ForgeConfigSpec SPEC = BUILDER.build();
 
     public static Pattern regexPattern;
 
